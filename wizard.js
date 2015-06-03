b5a8ba17ca93f1141149ede2b1927e7941f1491b
Hossam Hazem <hossamhazem94@gmail.com>
2015-06-03 21:10:13 +0200

    animations added

--- wizard.js
+++ wizard.js
@@ -2,8 +2,15 @@ $(document).ready(function(){
 	var counter=0;
 	var $current=$('#p'+counter)
 	$current.addClass('current');
+	var animend=whichTransitionEvent();
 	var numberOfPages=$(".main").children('.slide').length;
-
+	animEndEventNames = {
+			'WebkitAnimation' : 'webkitAnimationEnd',
+			'OAnimation' : 'oAnimationEnd',
+			'msAnimation' : 'MSAnimationEnd',
+			'animation' : 'animationend'
+		},
+		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ]
 
 	$('.prevbt').click(function(){
 		$current.removeClass('current')
@@ -18,10 +25,25 @@ $(document).ready(function(){
 		counter=nextpagecounter(counter);
 		console.log(counter)
 		$next=$('#p'+counter);
-		$next.addClass('current');
-		$current.removeClass('current');
-		$current=$next;
+		$current.addClass('SlideOut').on(animEndEventName,function(){
+			console.log('end c')
+			$current.off(animEndEventName)
+			
+		})
 
+			$next.addClass('current');
+			$next.addClass('SlideIn');
+		$next.on(animEndEventName,function(){
+				console.log('end n');
+				$current.removeClass('current');
+				$current.off(animEndEventName);
+				$next.off(animEndEventName);
+				$current.removeClass('SlideOut')
+				$next.removeClass('SlideIn')
+				
+				$next.off(animEndEventName);
+				$current=$next;
+			})
 		
 	})
 })
@@ -33,4 +55,20 @@ function nextpagecounter(current){
 function prevpagecounter(current){
 	var numberOfPages=$(".main").children('.slide').length;
 	return (((current-1)%numberOfPages)+numberOfPages)%numberOfPages;
+}
+function whichTransitionEvent(){
+    var t;
+    var el = document.createElement('fakeelement');
+    var transitions = {
+      'transition':'transitionend',
+      'OTransition':'oTransitionEnd',
+      'MozTransition':'transitionend',
+      'WebkitTransition':'webkitTransitionEnd'
+    }
+
+    for(t in transitions){
+        if( el.style[t] !== undefined ){
+            return transitions[t];
+        }
+    }
 }
